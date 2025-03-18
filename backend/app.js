7f8444e82bb80d7969cccf4c0c73ca9bb750f77e
MercedesAAlvarez <106325676+MercedesAAlvarez@users.noreply.github.com>
2025-03-18 08:37:52 -0300

    instalacion de dependencias

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+
+const express = require('express') 
+let path = require ('path')
+const app = express()
+const port = 3017
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Servidor levantado en http://localhost:${port}\n`))
