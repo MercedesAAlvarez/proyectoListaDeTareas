cccdcdf075f2e5fcc210b16056243903b93cf716
MercedesAAlvarez <106325676+MercedesAAlvarez@users.noreply.github.com>
2025-03-18 18:07:18 -0300

    continuando con modificaciones y pruebas

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-/* let path = require('path'); */
+let path = require('path');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
@@ -24,11 +24,14 @@ app.get("/api/tasks",(req,res)=>{
 app.post("/api/tasks",(req,res)=>{
     const {title,description} =req.body;
 
-    if (!title?.trim()||!description?.trim()){
+   /*  if (!title?.trim()||!description?.trim()){
+    return res.status(400).json({error: "Tenes que ingresar un titulo y una descripcion"})
+    } */
+    if (title === undefined||description === undefined){
     return res.status(400).json({error: "Tenes que ingresar un titulo y una descripcion"})
     }
      
-    const newTask ={
+    const taskNueva ={
     id: id++,
     title,
     description,
@@ -36,15 +39,16 @@ app.post("/api/tasks",(req,res)=>{
     createAt: new Date()
     };
 
-    tasks.push(newTask);
-    res.status(201).json(newTask)
+    tasks.push(taskNueva);
+    res.status(201).json(taskNueva)
 
     });
 
    /*  Actualizar tarea */
 
    app.put("/api/tasks/:id", (req, res) => {
-    const id = Number(req.params.id);
+    /* const id = Number(req.params.id); */
+    const id = +req.params;
     const {title,description,completed} = req.body;
     const taskId = tasks.findIndex(task => task.id === id);
 
@@ -52,7 +56,10 @@ app.post("/api/tasks",(req,res)=>{
       return res.status(404).json({error:" No se encontro la tarea,probá nuevamente"})
     }
 
-    if (!title?.trim() || !description?.trim()||completed === undefined) {
+   /*  if (!title?.trim() || !description?.trim()||completed === undefined) {
+     return res.status(400).json({error:"Todos los campos son obligatorios"})
+    } */
+    if (title.lenght === 0 || description.lenght === 0 ||completed === undefined) {
      return res.status(400).json({error:"Todos los campos son obligatorios"})
     }
     
@@ -69,14 +76,15 @@ app.post("/api/tasks",(req,res)=>{
 /* Borrar tarea */
 
 app.delete("/api/tasks/:id", (req, res) => {
-    const id = Number(req.params.id);
+   /*  const id = Number(req.params.id); */
+    const id = +req.params;
     const taskFind = tasks.find(task => task.id === id)
      if (taskFind === undefined){
-        return res.status(404).json({error:" La Tarea no se ha encontrado o no existe"})
+        return res.status(404).json({error:" La tarea no se ha encontrado o no existe"})
      }
 
      tasks= tasks.filter(task => task.id != id)
-      return res.json({message:"La tarea fue eliminada"})
+      return res.json({message:"La tarea fue eliminada exitosamnte"})
 });
 
 
